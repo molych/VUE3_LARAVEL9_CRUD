559d488bcebf3a8ccfb0371fe35ca1b501d0a739
Oleksandr Mochalov <molych69@gmail.com>
2022-06-25 16:40:31 +0300

    add delete method

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -101,6 +101,7 @@
                     </td>
                     <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
                         <router-link :to="{ name: 'posts.edit', params: { id: post.id } }">Edit</router-link>
+                        <a href="#" @click.prevent="deletePost(post.id)" class="ml-2">Delete</a>
                     </td>
                 </tr>
                 </tbody>
@@ -124,7 +125,7 @@ export default {
         const selectedCategory = ref('')
         const orderColumn = ref('created_at')
         const orderDirection = ref('desc')
-        const { posts, getPosts } = usePosts()
+        const { posts, getPosts,  deletePost } = usePosts()
         const { categories, getCategories } = useCategories()
         onMounted(() => {
             getPosts()
@@ -140,7 +141,17 @@ export default {
         watch(selectedCategory, (current, previous) => {
             getPosts(1, current)
         })
-        return { posts, getPosts, categories, selectedCategory, orderColumn, orderDirection, updateOrdering }
+
+         return {
+            posts,
+            getPosts,
+            deletePost,
+            categories,
+            selectedCategory,
+            orderColumn,
+            orderDirection,
+            updateOrdering
+        }
     }
 }
 </script>
